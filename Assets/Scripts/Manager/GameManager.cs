c3fbe77ff5beec5cdaf754d6e654b4e9764eb19f
Jiwon Park <64686238+tenvi34@users.noreply.github.com>
2024-12-13 21:00:06 +0900

    디자인 패턴 완료

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -1,18 +1,35 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : Singleton<GameManager>
 {
-    // Start is called before the first frame update
+    private DateTime _sessionStartTime;
+    private DateTime _sessionEndTime;
+
     void Start()
     {
-        
+        _sessionStartTime = DateTime.Now;
+        Debug.Log("Game Session Start : " + DateTime.Now);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnApplicationQuit()
     {
+        _sessionEndTime = DateTime.Now;
         
+        TimeSpan timeDiff = _sessionEndTime.Subtract(_sessionStartTime);
+        
+        Debug.Log("Game Session End : " + DateTime.Now);
+        Debug.Log("Game Session Time Lasted: " + timeDiff);
+    }
+
+    private void OnGUI()
+    {
+        if (GUILayout.Button("Next Scene"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
     }
 }
