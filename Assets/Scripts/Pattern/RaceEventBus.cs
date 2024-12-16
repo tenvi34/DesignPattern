8ca970f4da6f2ffe535e8e0024eaacc77cefef0e
Jiwon Park <64686238+tenvi34@users.noreply.github.com>
2024-12-16 23:02:48 +0900

    이벤트 타입 정의 완료

---
+++ Assets/Scripts/Pattern/RaceEventBus.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum RaceEventType
+{
+    COUNTDOWN,
+    START,
+    RESTART,
+    PAUSE,
+    STOP,
+    FINISH,
+    QUIT
+}
+
+public class RaceEventBus
+{
+    
+}
