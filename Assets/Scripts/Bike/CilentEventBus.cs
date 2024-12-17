98a7896d01c06049529952567b714b94170afb4d
Jiwon Park <64686238+tenvi34@users.noreply.github.com>
2024-12-17 22:44:47 +0900

    EventBus 패턴 완료

---
+++ Assets/Scripts/Bike/CilentEventBus.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class CilentEventBus : MonoBehaviour
+{
+    private bool _isButtonEnabled;
+    
+    void Start()
+    {
+        gameObject.AddComponent<HUDController>();
+        gameObject.AddComponent<CountdownTimer>();
+        gameObject.AddComponent<BikeController>();
+    }
+
+    private void OnEnable()
+    {
+        RaceEventBus.Subscribe(RaceEventType.STOP, Restart);
+    }
+
+    private void OnDisable()
+    {
+        RaceEventBus.UnSubscribe(RaceEventType.STOP, Restart);
+    }
+
+    private void Restart()
+    {
+        _isButtonEnabled = true;
+    }
+
+    private void OnGUI()
+    {
+        if (_isButtonEnabled)
+        {
+            if (GUILayout.Button("Start Countdown"))
+            {
+                _isButtonEnabled = false;
+                RaceEventBus.Publish(RaceEventType.COUNTDOWN);
+            }
+        }
+    }
+}
