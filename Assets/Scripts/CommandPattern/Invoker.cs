826163198e8580b595bc496116e32fb91036d5a3
Jiwon Park <64686238+tenvi34@users.noreply.github.com>
2024-12-20 23:16:47 +0900

    Invoker에 설명 주석 추가

--- Assets/Scripts/CommandPattern/Invoker.cs
+++ Assets/Scripts/CommandPattern/Invoker.cs
@@ -45,22 +45,30 @@ public class Invoker : MonoBehaviour
 
     private void FixedUpdate()
     {
+        // 녹화가 시작되면
         if (_isRecording)
         {
+            // FixedUpdate() 함수가 호출될 때마다 녹화 시간을 증가시킨다.
             _recordingTime += Time.fixedDeltaTime;
         }
 
+        // 리플레이 영상이 재생중이면
         if (_isReplaying)
         {
+            // 재생 시간이 매 프레임마다 증가한다.
             _replayTime += Time.deltaTime;
 
             if (_recordedCommands.Any())
             {
+                // _replayTime이 Keys[0](첫번째로 녹화된 시간)과 거의 같은지 확인 후
                 if (Mathf.Approximately(_replayTime, _recordedCommands.Keys[0]))
                 {
                     Debug.Log("Replay Time: " + _replayTime);
                     Debug.Log("Replay Command: " + _recordedCommands.Values[0]);
                     
+                    // Execute() 함수를 호출하고, 실행된 Command를 삭제한다.
+                    // TurnLeft 객체가 전달되면 TurnLeft 클래스의 Execute() 메서드가 호출되고,
+                    // TurnRight 객체가 전달되면 TurnRight 클래스의 Execute() 메서드가 호출된다.
                     _recordedCommands.Values[0].Execute();
                     _recordedCommands.RemoveAt(0);
                 }
