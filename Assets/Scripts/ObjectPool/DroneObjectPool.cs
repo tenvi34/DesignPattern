4f71f011c0227cc3f5240852fbbbd1d54b24aa82
Jiwon Park <64686238+tenvi34@users.noreply.github.com>
2024-12-22 20:05:29 +0900

    설명 주석 추가

--- Assets/Scripts/ObjectPool/DroneObjectPool.cs
+++ Assets/Scripts/ObjectPool/DroneObjectPool.cs
@@ -30,6 +30,7 @@ namespace ObjectPool
         
         private IObjectPool<Drone> _pool;
         
+        // 드론 인스턴스 초기화, Pool에 드론 추가, 실제 사용할 때는 프리팹을 로드하는 방향으로 작성
         private Drone CreatePooledItem()
         {
             var go = GameObject.CreatePrimitive(PrimitiveType.Cube);
@@ -42,16 +43,19 @@ namespace ObjectPool
             return drone;
         }
         
+        // 클라이언트가 Pool에서 드론을 가져갈 때 호출, 실제로 반환되는 것이 아닌 게임 오브젝트가 활성화되는 방식
         private void OnTakeFromPool(Drone drone)
         {
             drone.gameObject.SetActive(true);
         }
 
+        // 실제로 반환되는 것이 아닌 게임 오브젝트가 비활성화되는 방식
         private void OnReturnedToPool(Drone drone)
         {
             drone.gameObject.SetActive(false);
         }
         
+        // Pool에 더이상 공간이 없을 때 호출, 반환되는 드론은 메모리 확보를 위해 파괴된다.
         private void OnDestroyToPool(Drone drone)
         {
             Destroy(drone.gameObject);
