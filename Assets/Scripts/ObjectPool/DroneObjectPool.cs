f6bf022a4b5784963844eba9d2093a35de8ea81c
Jiwon Park <64686238+tenvi34@users.noreply.github.com>
2024-12-22 20:00:59 +0900

    드론 오브젝트 풀 관리 스크립트 생성

---
+++ Assets/Scripts/ObjectPool/DroneObjectPool.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using UnityEngine.Pool;
+
+namespace ObjectPool
+{
+    public class DroneObjectPool : MonoBehaviour
+    {
+        public int maxPoolSize = 10;
+        public int stackDefaultCapacity = 10;
+
+        public IObjectPool<Drone> Pool
+        {
+            get
+            {
+                if (_pool == null)
+                {
+                    _pool = new ObjectPool<Drone>(
+                        CreatePooledItem, 
+                        OnTakeFromPool, 
+                        OnReturnedToPool,
+                        OnDestroyToPool, 
+                        true, 
+                        stackDefaultCapacity, 
+                        maxPoolSize);
+                }
+
+                return _pool;
+            }
+        }
+        
+        private IObjectPool<Drone> _pool;
+        
+        private Drone CreatePooledItem()
+        {
+            var go = GameObject.CreatePrimitive(PrimitiveType.Cube);
+
+            Drone drone = go.AddComponent<Drone>();
+
+            go.name = "Drone";
+            drone.Pool = Pool;
+
+            return drone;
+        }
+        
+        private void OnTakeFromPool(Drone drone)
+        {
+            drone.gameObject.SetActive(true);
+        }
+
+        private void OnReturnedToPool(Drone drone)
+        {
+            drone.gameObject.SetActive(false);
+        }
+        
+        private void OnDestroyToPool(Drone drone)
+        {
+            Destroy(drone.gameObject);
+        }
+
+        public void Spawn()
+        {
+            var amount = Random.Range(1, 10);
+
+            for (int i = 0; i < amount; i++)
+            {
+                var drone = Pool.Get();
+                drone.transform.position = Random.insideUnitSphere * 10;
+            }
+        }
+    }
+}
