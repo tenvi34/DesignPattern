7b6a4d3d99e5b3fbb1a2c1ef1a5ea63b14a3f9d8
Jiwon Park <64686238+tenvi34@users.noreply.github.com>
2024-12-23 16:54:59 +0900

    옵저버와 Subject 틀 구현

---
+++ Assets/Scripts/ObserverPattern/BikeController.cs
@@ -0,0 +1,7 @@
+﻿namespace ObserverPattern
+{
+    public class BikeController : Subject
+    {
+        
+    }
+}
---
+++ Assets/Scripts/ObserverPattern/Subject.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using UnityEngine;
+
+namespace ObserverPattern
+{
+    public class Subject : MonoBehaviour
+    {
+        private readonly ArrayList _observers = new ArrayList();
+
+        public void Attach(Observer observer)
+        {
+            _observers.Add(observer);
+        }
+
+        public void Detach(Observer observer)
+        {
+            _observers.Remove(observer);
+        }
+
+        public void NotifyObservers()
+        {
+            foreach (Observer observer in _observers)
+            {
+                observer.Notify(this);
+            }
+        }
+    }
+}
