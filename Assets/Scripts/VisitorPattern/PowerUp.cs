133eb24d8777e93782c6befc6a57d94b5c30d08c
Jiwon Park <64686238+tenvi34@users.noreply.github.com>
2024-12-30 11:34:56 +0900

    PowerUp 개별 Visit() 구현 완료

--- Assets/Scripts/VisitorPattern/PowerUp.cs
+++ Assets/Scripts/VisitorPattern/PowerUp.cs
@@ -18,7 +18,7 @@ namespace VisitorPattern
 
         [Range(0.0f, 25f)] 
         [Tooltip("Boost weapon range in increments of up to 25 units")]
-        public float weaponRange;
+        public int weaponRange;
         
         [Range(0.0f, 50f)]
         [Tooltip("Boost weapon strength in increments of up to 50%")]
@@ -31,12 +31,29 @@ namespace VisitorPattern
 
         public void Visit(BikeEngine bikeEngine)
         {
+            float boost = bikeEngine.turboBoost += turboBoost;
+
+            if (boost < 0.0f) bikeEngine.turboBoost = 0.0f;
             
+            if (boost >= bikeEngine.maxTurboBoost)
+                bikeEngine.turboBoost = bikeEngine.maxTurboBoost;
         }
 
         public void Visit(BikeWeapon bikeWeapon)
         {
+            // range
+            int range = bikeWeapon.range += weaponRange;
+            if (range >= bikeWeapon.maxRange)
+                bikeWeapon.range = bikeWeapon.maxRange;
+            else
+                bikeWeapon.range = range;
             
+            // strength
+            float strength = bikeWeapon.strength += Mathf.Round(bikeWeapon.strength * weaponStrength / 100);
+            if (strength >= bikeWeapon.maxStrength)
+                bikeWeapon.strength = bikeWeapon.maxStrength;
+            else
+                bikeWeapon.strength = strength;
         }
     }
 }
