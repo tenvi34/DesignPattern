5388eb28d9989c2fa78fe2477fdcd105a123be32
Jiwon Park <64686238+tenvi34@users.noreply.github.com>
2024-12-30 17:29:41 +0900

    방문자 패턴 예제 완성 및 테스트

---
+++ Assets/Scripts/VisitorPattern/ClientVisitor.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+
+namespace VisitorPattern
+{
+    public class ClientVisitor : MonoBehaviour
+    {
+        public PowerUp enginePowerUp;
+        public PowerUp shieldPowerUp;
+        public PowerUp weaponPowerUp;
+        
+        private BikeController _bikeController;
+
+        private void Start()
+        {
+            _bikeController = gameObject.AddComponent<BikeController>();
+        }
+
+        private void OnGUI()
+        {
+            if (GUILayout.Button("Engine Power Up"))
+            {
+                _bikeController.Accept(enginePowerUp);
+            }
+            
+            if (GUILayout.Button("Shield Power Up"))
+            {
+                _bikeController.Accept(shieldPowerUp);
+            }
+            
+            if (GUILayout.Button("Weapon Power Up"))
+            {
+                _bikeController.Accept(weaponPowerUp);
+            }
+        }
+    }
+}
